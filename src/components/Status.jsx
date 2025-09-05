67c9e863af4c0f08df32eeaf7b1a0f1744361607
vitalista <vitalista.ariesg.gmail.com>
2025-09-05 07:08:57 -0700

    initial Status && Input component

---
+++ src/components/Status.jsx
@@ -0,0 +1,42 @@
+import { Info, CheckCircle2, AlertTriangle, AlertCircle } from "lucide-react";
+
+export default function Status({ type = "info", message = "Message here"}) {
+  let icon, classes;
+
+  switch (type) {
+    case "success":
+      icon = <CheckCircle2 className="h-5 w-5 text-emerald-600 opacity-90" />;
+      classes =
+        "ring-emerald-300/40 bg-emerald-400/10 text-emerald-900/90";
+      break;
+    case "warning":
+      icon = <AlertTriangle className="h-5 w-5 text-amber-600 opacity-90" />;
+      classes = "ring-amber-300/40 bg-amber-400/10 text-amber-900/90";
+      break;
+    case "error":
+      icon = <AlertCircle className="h-5 w-5 text-rose-600 opacity-90" />;
+      classes = "ring-rose-300/40 bg-rose-400/10 text-rose-900/90";
+      break;
+    default:
+      icon = <Info className="h-5 w-5 text-blue-600 opacity-90" />;
+      classes = "ring-blue-300/40 bg-blue-400/10 text-blue-900/90";
+  }
+
+  return (
+    <div
+      className={`m-3
+        w-full max-w-4xl rounded-2xl border border-white/20 bg-white/10 backdrop-blur-md shadow-lg ring-1 p-4 ${classes} 
+      `
+    }
+    >
+      <div className="flex items-start gap-3">
+        {icon}
+        <div className="flex-1">
+          {message && (
+            <p className="text-sm text-slate-800/90">{message}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/page/register_acc.jsx
+++ src/page/register_acc.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import Sidebar from "../components/sidebar";
 import Navbar from "../components/navbar.jsx";
+import { useMediaQuery } from "react-responsive";
+import Input from "../components/Input";
+import api from "../js/axiosInstance.js";
+import Status from "../components/Status.jsx";
+
 export default function RegisterAccount() {
   const currentWebPage = "Account Registration";
 
@@ -10,24 +15,45 @@ export default function RegisterAccount() {
     setMobileSideBar(state);
   };
 
+  const IsSmallMobile = useMediaQuery({ maxWidth: 768 });
+
   return (
-    <div>
-      <div className="flex items-center  min-h-screen bg-gray-100">
-        <Navbar
-          page={currentWebPage}
-          setSideBarMobile={HandleMobileSideBar}
-          mobileSideBarState={MobileSideBar}
-        />
-        <Sidebar SideBarMobileState={MobileSideBar} />
-        {/*Dito lagay yung page layout*/}
-        <div className="flex w-1/1  justify-center">
-          <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md w-full text-center m-6 ">
-            <h1 className="text-3xl font-bold text-gray-800 mb-4">
-              📊 Register Account Page
-            </h1>
+    <div className="w-1/1 h-screen flex">
+      <Navbar
+        page={currentWebPage}
+        setSideBarMobile={HandleMobileSideBar}
+        mobileSideBarState={MobileSideBar}
+      />
+      <Sidebar SideBarMobileState={MobileSideBar} />
+      {/*Dito lagay yung page layout*/}
+      <main
+        className={`flex-1 px-0  w-100   ${IsSmallMobile ? `mt-15` : `mt-15`}`}
+      >
+        <div
+          className={`rounded-2xl w-full h-1/1 ${IsSmallMobile ? `p-1` : `p-3`
+            }`}
+        >
+
+          <Status />
+          <Status type="error" />
+          <Status type="warning" />
+          <Status type="success" />
+          
+
+          <div className="flex flex-col sm:flex-row gap-4 p-10">
+            <div className="flex-1 flex flex-col">
+              <label className="mb-1 text-sm font-medium text-slate-700 ms-2">First Name</label>
+              <Input type="text" placeholder="Enter first name" />
+            </div>
+
+            <div className="flex-1 flex flex-col">
+              <label className="mb-1 text-sm font-medium text-slate-700 ms-2">Last Name</label>
+              <Input type="text" placeholder="Enter last name" />
+            </div>
           </div>
+
         </div>
-      </div>
+      </main>
     </div>
   );
 }
