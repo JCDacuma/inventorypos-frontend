356a0eb511b63a0056e254b899c3d93b39718b4b
vitalista <vitalista.ariesg@gmail.com>
2025-09-07 05:21:01 -0700

    test author

--- src/page/config_acc.jsx
+++ src/page/config_acc.jsx
@@ -4,14 +4,7 @@ export default function ConfigAccount() {
 
   return (
     <Layout currentWebPage="Account Configuration" style="items-center">
-      {/*Dito lagay yung page layout*/}
-      <div className="flex w-1/1  justify-center">
-        <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md w-full text-center m-6 ">
-          <h1 className="text-3xl font-bold text-gray-800 mb-4">
-            📊 Config Report Page
-          </h1>
-        </div>
-      </div>
+      
     </Layout>
 
   );
